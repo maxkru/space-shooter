85a6e8755dcd96d8ad106c066f76689fd71a2bb7
maxkru <max_kru@hotmail.com>
2020-02-17 13:36:19 +0300

    Initial commit

---
+++ desktop/src/kriuchkov/maksim/spaceshooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package kriuchkov.maksim.spaceshooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import kriuchkov.maksim.spaceshooter.SpaceShooter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceShooter(), config);
+	}
+}
